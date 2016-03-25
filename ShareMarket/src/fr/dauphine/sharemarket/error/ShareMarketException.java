f6687e8f878fa28d492e6565fc0a939a774ec0a8
Mathias Pereira <mattdu94310@hotmail.fr>
2016-03-25 22:54:02 +0100

    preparation du merge

--- ShareMarket/src/fr/dauphine/sharemarket/error/ShareMarketException.java
+++
@@ -1,8 +0,0 @@
-package fr.dauphine.sharemarket.error;
-public class ShareMarketException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public ShareMarketException(String msg) {
-		super(msg);
-	}
-}
